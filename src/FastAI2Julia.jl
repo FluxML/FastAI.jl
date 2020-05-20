1fb1cbee868185fd7dff072f5b59dd9be337006d
peter.wolf <peter.wolf@englishcentral.com>
2020-05-20 19:25:02 -0400

    Update doc in FastAI2Julia.jl

--- src/FastAI2Julia.jl
+++ src/FastAI2Julia.jl
@@ -1,3 +1,22 @@
+#=
+FastAI2Julia.jl:
+
+Author: Peter Wolf (opus111@gmail.com)
+
+A first cut at a port of the FastAI V2 API to Julia
+
+The original source is here
+
+https://github.com/fastai/fastai2/blob/master/fastai2/
+
+The documentation is copied from here
+
+https://github.com/fastai/fastai2/blob/master/docs/
+
+The main purpose of this code is to see if the team likes the method
+of defining an interface and implementations in Julia
+=#
+
 module FastAI2Julia
 
 export fit
