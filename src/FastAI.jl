ca66e8efaa27b2c42bb9c5404c942c9fed9b66ad
lorenzoh <lorenz.ohly@gmail.com>
2021-01-22 11:07:33 +0100

    .

--- src/FastAI.jl
+++ src/FastAI.jl
@@ -1,20 +1,32 @@
 module FastAI
 
+using DLDatasets
 using DLPipelines
+using DLPipelines: methodmodel, methoddataset, methoddataloaders, ImageClassification
 using DataAugmentation
 using DataLoaders
 using Flux
 using FluxTraining
+using MLDataPattern
+
+const Datasets = DLDatasets
 
 
 include("training.jl")
 
 
 export
+    # submodules
+    Datasets,
+    loaddataset,
+
     # method API
     methodmodel,
     methoddataset,
     methoddataloaders,
+
+    # methods
+    ImageClassification,
 
     # training
     fit!,
