709fa647e6aace65d8bfa1ddba3414de593084a6
lorenzoh <lorenz.ohly@gmail.com>
2021-02-20 13:34:50 +0100

    improve doc ci

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,13 @@ using FastAI
 using Pkg.Artifacts
 
 
+# Download theme
+download_artifact(
+    Base.SHA1("6e4be8bec8da9323c18d777d7855ef79dddcf524"),
+    "https://github.com/darsnack/flux-theme/releases/download/v0.2.1/flux-theme-0.2.1.tar.gz",
+    "b00941248ecdb643a72960bbfda19a3128591eca3d7cbcb3bfb80a6ab1c7f99e",
+)
+
 Publish.Themes.default() = artifact"flux-theme"
 
 p = Publish.Project(FastAI)
