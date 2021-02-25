84e647dc4be1ecf65e38aed1e3c6073e0387f45b
lorenzoh <lorenz.ohly@gmail.com>
2021-02-25 20:42:47 +0100

    up

--- dev/versions.js
+++ dev/versions.js
@@ -2,7 +2,5 @@ var PUBLISH_ROOT = '/FastAI.jl/dev';
 var PUBLISH_VERSION = "dev";
 var PUBLISH_VERSIONS = [
     ["dev","/FastAI.jl/dev/index.html"],
-    ["FluxTraining.jl","/FastAI.jl/FluxTraining.jl/index.html"],
-    ["FastAI.jl","/FastAI.jl/FastAI.jl/index.html"],
     ["0.1.0","/FastAI.jl/0.1.0/index.html"],
 ];
