4b245908a0a48d3fbe53a9156bd539985d7359be
lorenzoh <lorenz.ohly@gmail.com>
2021-03-14 13:33:22 +0100

    move to Pollen

--- 0.1.0/versions.js
+++
@@ -1,6 +0,0 @@
-var PUBLISH_ROOT = '/FastAI.jl/0.1.0';
-var PUBLISH_VERSION = "0.1.0";
-var PUBLISH_VERSIONS = [
-    ["dev","/FastAI.jl/dev/index.html"],
-    ["0.1.0","/FastAI.jl/0.1.0/index.html"],
-];
