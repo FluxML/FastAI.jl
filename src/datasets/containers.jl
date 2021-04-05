19a77acfbaf3bf16d6c56bd672300c2d1c30de26
manikyabard <manikyabard@gmail.com>
2021-04-05 15:27:52 +0000

    added TableDataset

--- src/datasets/containers.jl
+++ src/datasets/containers.jl
@@ -1,4 +1,6 @@
-
+using DataFrames
+using Tables
+using CSV
 
 # FileDataset
 
@@ -46,4 +48,36 @@ isimagefile(file::String) = occursin(IMAGEFILE_REGEX, lowercase(file))
 const IMAGEFILE_REGEX = r"\.(gif|jpe?g|tiff?|png|webp|bmp)$"
 
 
-## TODO: TableDataset
+struct TableDataset{T}
+    table::T #Should implement Tables.jl interface
+    TableDataset{T}(table::T) where T = Tables.istable(table) ? new{T}(table) : error("Object doesn't implement Tables.jl interface")
+end
+
+TableDataset(path::String) = TableDataset(DataFrame(CSV.File(path)))
+TableDataset(table::T) where {T} = TableDataset{T}(table)
+
+function LearnBase.getobs(dataset::FastAI.Datasets.TableDataset{T}, idx) where {T}
+    if Tables.rowaccess(dataset.table)
+        for (index, row) in enumerate(Tables.rows(dataset.table))
+            if index==idx
+                return row
+            end
+        end
+    elseif Tables.columnaccess(dataset.table)
+        rowvals = []
+        for i in 1:(length ∘ Tables.columnnames)(dataset.table)
+            append!(rowvals, Tables.getcolumn(dataset.table, i)[idx])
+        end
+        return rowvals
+    else error("The Tables.jl implementation used should have either rowaccess or columnaccess.")
+    end
+end
+
+function LearnBase.nobs(dataset::FastAI.Datasets.TableDataset{T}) where {T}
+    if Tables.rowaccess(dataset.table)
+        return length(Tables.rows(dataset.table))
+    elseif Tables.columnaccess(dataset.table)
+        return length(Tables.getcolumn(dataset.table, 1))
+    else error("The Tables.jl implementation used should have either rowaccess or columnaccess.")
+    end
+end
