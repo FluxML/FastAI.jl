378e6ef91907df53281740261afcf6e182fea20a
lorenzoh <lorenz.ohly@gmail.com>
2021-04-17 15:23:58 +0200

    move docs to Pollen

--- docs/serve.jl
+++ docs/serve.jl
@@ -5,7 +5,6 @@ using DLPipelines
 using DataAugmentation
 using FilePathsBase
 
-
 refmodules = [FluxTraining, DLPipelines, DataAugmentation, FastAI.Datasets, FastAI]
-project = Pollen.documentationproject(FastAI; refmodules = refmodules)
-Pollen.fullbuild(project, Pollen.FileBuilder(Pollen.HTML(), p"dev/"))
+project = Pollen.documentationproject(FastAI; refmodules, inlineincludes = false)
+Pollen.serve(project)
