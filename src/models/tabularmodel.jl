c0b2922f27996763f03b3360e83354a4aaa39c85
Manikya <manikyabard@gmail.com>
2021-06-20 01:53:01 +0530

    simplified tabular model

--- src/models/tabularmodel.jl
+++ src/models/tabularmodel.jl
@@ -19,34 +19,38 @@ end
 # end
 
 function TabularModel(
-		layers; 
-		emb_szs,
-		n_cont::Int64,
-		out_sz,
-		ps::Union{Tuple, Vector, Number}=0,
-		embed_p::Float64=0.,
-		y_range=nothing,
-		use_bn::Bool=true,
-		bn_final::Bool=false,
-		bn_cont::Bool=true,
-		act_cls=Flux.relu,
-		lin_first::Bool=true)
+        layers; 
+        emb_szs,
+        n_cont::Int64,
+        out_sz,
+        ps::Union{Tuple, Vector, Number}=0,
+        embed_p::Float64=0.,
+        y_range=nothing,
+        use_bn::Bool=true,
+        bn_final::Bool=false,
+        bn_cont::Bool=true,
+        act_cls=Flux.relu,
+        lin_first::Bool=true)
 
-	embedslist = [Embedding(ni, nf) for (ni, nf) in emb_szs]
-	emb_drop = Dropout(embed_p)
-	embeds = Chain(x -> ntuple(i -> x[i, :], length(emb_szs)), Parallel(vcat, embedslist...), emb_drop)
+    embedslist = [Embedding(ni, nf) for (ni, nf) in emb_szs]
+    emb_drop = Dropout(embed_p)
+    embeds = Chain(x -> ntuple(i -> x[i, :], length(emb_szs)), Parallel(vcat, embedslist...), emb_drop)
 
-	bn_cont = bn_cont ? BatchNorm(n_cont) : identity
+    bn_cont = bn_cont ? BatchNorm(n_cont) : identity
 
-	n_emb = sum(size(embedlayer.weight)[1] for embedlayer in embedslist)
-	sizes = append!(zeros(0), [n_emb+n_cont], layers)
+    n_emb = sum(size(embedlayer.weight)[1] for embedlayer in embedslist)
 
-	_layers = []
-	for (i, (p, a)) in enumerate(zip(Iterators.cycle(ps), actns))
-		layer = linbndrop(Int64(sizes[i]), Int64(sizes[i+1]), use_bn=use_bn, p=p, act=act_cls, lin_first=lin_first)
-		push!(_layers, layer)
-	end
-	push!(_layers, linbndrop(Int64(last(sizes)), Int64(out_sz), use_bn=bn_final, lin_first=lin_first))
-	layers = isnothing(y_range) ? Chain(Parallel(vcat, embeds, bn_cont), _layers...) : Chain(Parallel(vcat, embeds, bn_cont), _layers..., @. x->Flux.sigmoid(x) * (y_range[2] - y_range[1]) + y_range[1])
-	layers
+    ps = Iterators.cycle(ps)
+    classifiers = []
+
+    first_ps, ps = Iterators.peel(ps)
+    push!(classifiers, linbndrop(n_emb+n_cont, first(layers); use_bn=use_bn, p=first_ps, lin_first=lin_first, act=act_cls))
+    for (isize, osize, p) in zip(layers[1:(end-1)], layers[2:(end)], ps)
+        layer = linbndrop(isize, osize; use_bn=use_bn, p=p, act=act_cls, lin_first=lin_first)
+        push!(classifiers, layer)
+    end
+    push!(classifiers, linbndrop(last(layers), out_sz; use_bn=bn_final, lin_first=lin_first))
+
+    layers = isnothing(y_range) ? Chain(Parallel(vcat, embeds, bn_cont), classifiers...) : Chain(Parallel(vcat, embeds, bn_cont), classifiers..., @. x->Flux.sigmoid(x) * (y_range[2] - y_range[1]) + y_range[1])
+    layers
 end
