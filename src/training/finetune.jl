356e20ce5881bd9a6d22df72f8e92c0c17dec9f4
lorenzoh <lorenz.ohly@gmail.com>
2021-07-03 16:32:16 +0200

    update  `finetune!` and notebooks

--- src/training/finetune.jl
+++ src/training/finetune.jl
@@ -2,37 +2,32 @@
 """
     finetune!(learner, nepochs[, base_lr = 0.002; kwargs...])
 
-Behaves as [`fastai.Learner.fine_tune`](https://github.com/fastai/fastai/blob/master/fastai/callback/schedule.py#L151)
+Behaves like the fastai implementation
+[`fastai.Learner.fine_tune`](https://github.com/fastai/fastai/blob/master/fastai/callback/schedule.py#L151).
+
 """
 function finetune!(
         learner,
         nepochs,
         base_lr = 0.002;
         freezeepochs = 1,
-        grouper = nothing,
-        lr_mult = 10.,
+        grouper = defaultgrouper(learner.model),
+        backbone_factor = 1/10,
         div = 5,
         pct_start = 0.3,
         kwargs...)
 
-    # Parse arguments and throw helpful errors
-    grouper = isnothing(grouper) ? _init_grouper(learner.model) : grouper
-
-    FluxTraining.initlearner!(learner, [TrainingPhase()])
-
     # Freeze backbone and train head
-    foptim = freezeoptimizer(learner.optimizer, grouper, learner.model)
+    foptim = frozen_optimizer(learner.optimizer, grouper, learner.model)
     withfields(learner, optimizer = foptim) do
         fitonecycle!(learner, freezeepochs, base_lr, pct_start=0.99; kwargs...)
     end
 
-
     # Use discriminative learning rates on backbone and train some more
-    doptim = discrlr_optimizer(learner.optimizer, grouper, learner.model, lr_mult)
+    doptim = discrlr_optimizer(learner.optimizer, grouper, learner.model, backbone_factor)
     withfields(learner, optimizer = doptim) do
-        base_lr /= 2
         fitonecycle!(
-            learner, nepochs, base_lr;
+            learner, nepochs, base_lr / 2;
             div = div, pct_start = pct_start, kwargs...)
     end
 
@@ -40,7 +35,7 @@ function finetune!(
 end
 
 
-function freezeoptimizer(optim, grouper, model)
+function frozen_optimizer(optim, grouper, model)
     paramgroups = ParamGroups(grouper, model)
     return Optimiser(
         DiscriminativeLRs(paramgroups, Dict(1 => 0., 2 => 1.)),
@@ -58,7 +53,7 @@ function discrlr_optimizer(optim, grouper, model, factor)
 end
 
 
-function _init_grouper(model)
+function defaultgrouper(model)
     if !(model isa Chain)
         error(
             "Cannot freeze `learner.model` automatically since it is not a `Chain`.
