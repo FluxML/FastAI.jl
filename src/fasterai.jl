d011c6fef504e5f71e55b8d3a2026efd3a4ad6ac
lorenzoh <lorenz.ohly@gmail.com>
2021-08-10 14:18:39 +0200

    move file

--- src/fasterai.jl
+++
@@ -1,7 +0,0 @@
-defaultdataregistry() = Datasets.FASTAI_DATA_REGISTRY
-defaultmethodregistry() = LearningMethodRegistry()
-
-
-Datasets.listdatasources() = listdatasources(defaultdataregistry())
-Datasets.finddatasets(; kwargs...) = finddatasets(defaultdataregistry(); kwargs...)
-Datasets.loaddataset(args...; kwargs...) = loaddataset(defaultdataregistry(), args...; kwargs...)
