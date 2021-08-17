ddb4d620d71867e26008f306612624d968ebd81c
Manikya Bardhan <manikyabard@gmail.com>
2021-08-17 07:35:22 +0530

    made methods concise
    Co-authored-by: Kyle Daruwalla <daruwalla.k.public@icloud.com>

--- src/models/tabularmodel.jl
+++ src/models/tabularmodel.jl
@@ -5,16 +5,14 @@ Returns an embedding size corresponding to the number of classes for a
 categorical variable using the rule of thumb present in python fastai.
 (see https://github.com/fastai/fastai/blob/2742fe844573d06e700f869839fb9ec5f3a9bca9/fastai/tabular/model.py#L12)
 """
-function emb_sz_rule(n_cat)
-    min(600, round(1.6 * n_cat^0.56))
-end
+emb_sz_rule(n_cat) = min(600, round(Int, 1.6 * n_cat^0.56))
 
 """
     get_emb_sz(cardinalities, [size_overrides])
     get_emb_sz(cardinalities; catcols, [size_overrides])
 
 Returns a collection of tuples containing embedding dimensions corresponding to 
-number of classes in categorical columns present in `cardinalities` and adjusting for nans. 
+number of classes in categorical columns present in `cardinalities` and adjusting for NaNs. 
 
 ## Keyword arguments
 
@@ -25,12 +23,11 @@ number of classes in categorical columns present in `cardinalities` and adjustin
 - `categorical_cols`: A collection of categorical column names.
 """
 
-function get_emb_sz(cardinalities, size_overrides=fill(nothing, length(cardinalities)))
-    map(Iterators.enumerate(cardinalities)) do (i, cardinality)
-        emb_dim = isnothing(size_overrides[i]) ? emb_sz_rule(cardinality+1) : size_overrides[i]
-        (Int64(cardinality)+1, Int64(emb_dim))
+get_emb_sz(cardinalities::AbstractVector{<:Integer}, size_overrides=fill(nothing, length(cardinalities))) =
+    map(zip(cardinalities, size_overrides)) do (cardinality, override)
+        emb_dim = isnothing(override) ? emb_sz_rule(cardinality + 1) : Int64(override)
+        return (cardinality + 1, emb_dim)
     end
-end
 
 function get_emb_sz(cardinalities; catcols, size_overrides=Dict())
     keylist = keys(size_overrides)
@@ -40,9 +37,7 @@ function get_emb_sz(cardinalities; catcols, size_overrides=Dict())
     get_emb_sz(cardinalities, overrides)
 end
 
-function sigmoidrange(x, low, high)
-    @. Flux.sigmoid(x) * (high - low) + low
-end
+sigmoidrange(x, low, high) = @. Flux.sigmoid(x) * (high - low) + low
 
 function tabular_embedding_backbone(embedding_sizes, dropoutprob=0.)
     embedslist = [Flux.Embedding(ni, nf) for (ni, nf) in embedding_sizes]
@@ -54,13 +49,11 @@ function tabular_embedding_backbone(embedding_sizes, dropoutprob=0.)
     )
 end
 
-function tabular_continuous_backbone(n_cont)
-    BatchNorm(n_cont)
-end
+tabular_continuous_backbone(n_cont) = BatchNorm(n_cont)
 
 """
     TabularModel(catbackbone, contbackbone, [finalclassifier]; kwargs...)
-    TabularModel(`n_cont::Number, outsize::Number[; kwargs...])
+    TabularModel(n_cont, outsize [; kwargs...])
 
 Create a tabular model which takes in a tuple of categorical values 
 (label or one-hot encoded) and continuous values. The default categorical backbone is
@@ -90,7 +83,7 @@ function TabularModel(
         catbackbone, 
         contbackbone;
         outsize,
-        layersizes=[200, 100],
+        layersizes=(200, 100),
         kwargs...)
     TabularModel(catbackbone, contbackbone, Dense(layersizes[end], outsize); layersizes=layersizes, kwargs...)
 end
@@ -113,7 +106,8 @@ function TabularModel(
     classifiers = []
 
     first_ps, dropout_rates = Iterators.peel(dropout_rates)
-    push!(classifiers, linbndrop(classifierin, first(layersizes); use_bn=batchnorm, p=first_ps, lin_first=linear_first, act=activation))
+    push!(classifiers, linbndrop(classifierin, first(layersizes);
+                                 use_bn=batchnorm, p=first_ps, lin_first=linear_first, act=activation))
 
     for (isize, osize, p) in zip(layersizes[1:(end-1)], layersizes[2:end], dropout_rates)
         layer = linbndrop(isize, osize; use_bn=batchnorm, p=p, act=activation, lin_first=linear_first)
@@ -130,7 +124,7 @@ end
 function TabularModel(
         n_cont::Number,
         outsize::Number,
-        layersizes=[200, 100];
+        layersizes=(200, 100);
         cardinalities,
         size_overrides=fill(nothing, length(cardinalities)))
     embedszs = get_emb_sz(cardinalities, size_overrides)
