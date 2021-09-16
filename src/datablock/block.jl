aa04e2aa96b9b101111df39ebe08654cbe11b2fd
lorenzoh <lorenz.ohly@gmail.com>
2021-09-16 15:04:38 +0200

    Improve docstrings for `Block`

--- src/datablock/block.jl
+++ src/datablock/block.jl
@@ -1,3 +1,9 @@
+"""
+    abstract type AbstractBlock
+
+Abstract supertype of all blocks. You should not subtype form this,
+but instead from [`Block`](#) or [`WrapperBlock`](#).
+"""
 abstract type AbstractBlock end
 
 
@@ -5,33 +11,37 @@ abstract type AbstractBlock end
 """
     abstract type Block
 
-Represents a kind of data used in machine learning.
+A block describes the meaning of a piece of data in the context of a learning task.
+For example, for supervised learning tasks, there is an input and a target
+and we want to learn to predict targets from inputs. Learning to predict a
+cat/dog label from 2D images is a supervised image classification task that can
+be represented with the `Block`s `Image{2}()` and `Label(["cat", "dog"])`.
 
-- Does not hold the data itself
+`Block`s are used in virtually every part of the high-level interfaces, from data
+processing over model creation to visualization.
+
+## Extending
+
+Consider the following when subtyping `Block`. A block
+
+- Does not hold observation data itself. Instead they are used in conjunction with
+  data to annotate it with some meaning.
 - If it has any fields, they should be metadata that cannot be
   derived from the data itself and is constant for every sample in
   the dataset. For example `Label` holds all possible classes which
   are constant for the learning problem.
 
+### Interfaces
 
-## Interface
+There are many interfaces that can be implemented for a `Block`. See the docstrings
+of each function for more info about how to implement it.
 
-Required:
-
-- `checkblock(block, data)` checks if `data` is compatible with `block`. For example
-    `checkblock(Image{2}(), img)` checks that `img` is a matrix with numeric or color
-    values. Defaults to `false`.
-
-Optional:
-
-- For plotting interface:
-    - `plotblock!(f, block)`. Plot `block` on Makie figure `f`.
-- For testing interface:
-    - `mockblock(block)`. Randomly generate an instance of `block`. Needed
-    to derive the testing interface for `BlockMethod`.
-- For training interface:
-    - [`blocklossfn`](#)`(pred_block, true_block)`
-    - [`blockmodel`](#)`(inblock, outblock, backbone)`
+- [`checkblock`](#)`(block, data)`: check whether a piece of data is a valid block
+- [`mockblock`](#)`(block)`: randomly generate a piece of data
+- [`blocklossfn`](#)`(predblock, yblock)`: loss function for comparing two blocks
+- [`blockmodel`](#)`(inblock, outblock[, backbone])`: construct a task-specific model
+- [`blockbackbone`](#)`(inblock)`: construct a backbone model that takes in specific data
+- [`plotblock!`](#)`(block, data)`: visualize block data
 
 """
 abstract type Block <: AbstractBlock end
@@ -41,7 +51,26 @@ abstract type Block <: AbstractBlock end
     checkblock(block, data)
     checkblock(blocks, datas)
 
-Check that `data` is compatible with `block`.
+Check whether `data` is compatible with `block`, returning a `Bool`.
+
+## Examples
+
+```julia
+checkblock(Image{2}(), rand(RGB, 16, 16)) == true
+```
+
+```julia
+checkblock(
+    (Image{2}(),        Label(["cat", "dog"])),
+    (rand(RGB, 16, 16), "cat"                ),
+) == true
+```
+
+## Extending
+
+An implementation of `checkblock` should be as specific as possible. The
+default method returns `false`, so you only need to implement methods for valid types
+and return `true`.
 """
 checkblock(::Block, data) = false
 
