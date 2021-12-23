f1432ee40da926374ec4c37ca3a04e8596ef80e1
lorenzoh <lorenz.ohly@gmail.com>
2021-12-23 19:05:16 +0100

    implement block invariants for basic blocks

--- src/blocks/continuous.jl
+++ src/blocks/continuous.jl
@@ -20,3 +20,42 @@ function blocklossfn(outblock::Continuous, yblock::Continuous)
     outblock.size == yblock.size || error("Sizes of $outblock and $yblock differ!")
     return Flux.Losses.mse
 end
+
+
+function invariant_checkblock(block::Continuous; blockname = "block", obsname = "obs")
+    return SequenceInvariant(
+        [
+            BooleanInvariant(
+                obs -> obs isa AbstractVector,
+                name = "`$obsname` should be an `AbstractVector`",
+                messagefn = obs -> """`$obsname` should be an `AbstractVector`, instead
+                got type `$(typeof(obs))`.
+                """
+            ),
+            BooleanInvariant(
+                obs -> length(obs) == block.size,
+                name = "length(`$obsname`) should be $(block.size)",
+                messagefn = obs -> """`$obsname` should have $(block.size) features, instead
+                found a vector with $(length(obs)) features.
+                """
+            ),
+            BooleanInvariant(
+                obs -> eltype(obs) <: Number,
+                name = "`eltype($obsname)` should be a subtype of `Number`",
+                messagefn = obs -> """Found a non-numerical element type $(eltype(obs))"""
+            ),
+        ],
+        "`$obsname` should be a valid `$(summary(block))`",
+        "",
+    )
+end
+
+
+@testset "Continuous [block]" begin
+    inv = invariant_checkblock(Continuous(5))
+
+    @test check(inv, zeros(5))
+    @test !(check(inv, "hi"))
+    @test !(check(inv, ["hi"]))
+    @test !(check(inv, [5]))
+end
--- src/blocks/label.jl
+++ src/blocks/label.jl
@@ -31,11 +31,23 @@ struct Label{T} <: Block
     classes::AbstractVector{T}
 end
 
-checkblock(label::Label{T}, obs::T) where T = obs ∈ label.classes
-mockblock(label::Label{T}) where T = rand(label.classes)::T
+checkblock(label::Label{T}, obs::T) where {T} = obs ∈ label.classes
+mockblock(label::Label{T}) where {T} = rand(label.classes)::T
 
 setup(::Type{Label}, data) = Label(unique(eachobs(data)))
 
+Base.summary(io::IO, ::Label{T}) where {T} = print(io, "Label{", T, "}")
+
+function invariant_checkblock(block::Label; blockname = "block", obsname = "obs")
+    return BooleanInvariant(
+        obs -> obs ∈ block.classes,
+        name = "`$obsname` should be a valid `$(summary(block))`",
+        messagefn = obs -> """`$obsname` should be one of the valid classes, i.e.
+            `$obsname ∈ $blockname.classes`. Instead got unknown class `$(sprint(show, obs))`.
+            Valid classes are:
+            `$(sprint(show, block.classes, context=:limit => true))`""",
+    )
+end
 
 """
     LabelMulti(classes)
@@ -65,34 +77,77 @@ struct LabelMulti{T} <: Block
     classes::AbstractVector{T}
 end
 
-function checkblock(label::LabelMulti{T}, v::AbstractVector{T}) where T
+function checkblock(label::LabelMulti{T}, v::AbstractVector{T}) where {T}
     return all(map(x -> x ∈ label.classes, v))
 end
 
 mockblock(label::LabelMulti) =
-    unique([rand(label.classes) for _ in 1:rand(1:length(label.classes))])
+    unique([rand(label.classes) for _ = 1:rand(1:length(label.classes))])
 
 
 setup(::Type{LabelMulti}, data) = LabelMulti(unique(eachobs(data)))
 
 
-InlineTest.@testset "Label [block]" begin
-    block = Label(["cat", "dog"])
-    InlineTest.@test FastAI.checkblock(block, "cat")
-    InlineTest.@test !(FastAI.checkblock(block, "horsey"))
+Base.summary(io::IO, ::LabelMulti{T}) where {T} = print(io, "LabelMulti{", T, "}")
 
-    targets = ["cat", "dog", "dog", "dog", "cat", "dog"]
-    block = setup(Label, targets)
-    InlineTest.@test block.classes == ["cat", "dog"]
+function invariant_checkblock(block::LabelMulti; blockname = "block", obsname = "obs")
+    return SequenceInvariant(
+        [
+            BooleanInvariant(
+                obs -> obs isa AbstractVector,
+                name = "`$obsname` should be an `AbstractVector`",
+                messagefn = obs -> """`$obsname` should be an `AbstractVector`, instead
+                got type `$(typeof(obs))`.
+                """
+            ),
+            BooleanInvariant(
+                obs -> all([y ∈ block.classes for y in obs]),
+                name = "Elements in `$obsname` should be valid classes",
+                messagefn = obs -> """`$obsname` should contain only valid classes,
+                    i.e. `∀ y ∈ $obsname: y ∈ $blockname.classes`.
+                    Instead got unknown classes `$(
+                        sprint(show, [y for y in obs if !(y ∈ block.classes)]))`.
+
+                    Valid classes are:
+                    `$(sprint(show, block.classes, context=:limit => true))`""",
+            ),
+        ],
+        "`$obsname` should be a valid `$(summary(block))`",
+        "",
+    )
 end
 
 
-InlineTest.@testset "LabelMulti [block]" begin
+# ## Tests
+
+@testset "Label [block]" begin
+    block = Label(["cat", "dog"])
+    @test FastAI.checkblock(block, "cat")
+    @test !(FastAI.checkblock(block, "horsey"))
+
+    targets = ["cat", "dog", "dog", "dog", "cat", "dog"]
+    block = setup(Label, targets)
+    @test block.classes == ["cat", "dog"]
+
+    inv = invariant_checkblock(Label([1, 2, 3]))
+    @test check(inv, 1)
+    @test !(check(inv, 0))
+end
+
+
+@testset "LabelMulti [block]" begin
     block = LabelMulti(["cat", "dog"])
-    InlineTest.@test FastAI.checkblock(block, ["cat"])
-    InlineTest.@test !(FastAI.checkblock(block, ["horsey", "cat"]))
+    @test FastAI.checkblock(block, ["cat"])
+    @test !(FastAI.checkblock(block, ["horsey", "cat"]))
 
     targets = ["cat", "dog", "dog", "dog", "cat", "dog"]
     block = setup(LabelMulti, targets)
-    InlineTest.@test block.classes == ["cat", "dog"]
+    @test block.classes == ["cat", "dog"]
+
+    inv = invariant_checkblock(block)
+    @test check(inv, ["cat", "dog"])
+    @test check(inv, [])
+    @test !(check(inv, "cat"))
+    @test !(check(inv, ["mouse"]))
+    @test !(check(inv, ["mouse", "cat"]))
 end
