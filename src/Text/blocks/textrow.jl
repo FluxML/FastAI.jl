08fbfceb1264be805f2ccba1fabbf216c2a6ee37
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-03-20 21:15:08 +0530

    textrow block added

--- src/Text/blocks/textrow.jl
+++ src/Text/blocks/textrow.jl
@@ -21,16 +21,51 @@ function TextRow(catcols, contcols, categorydict)
     TextRow{length(catcols),length(contcols)}(catcols, contcols, categorydict)
 end
 
-# function checkblock(block::TextRow{M,N}, x where {M,N,T<:Number}
-# end
+function checkblock(block::TextRow, x)
+    columns = Tables.columnnames(x)
+    (
+        all(col -> col ∈ columns, (block.catcols..., block.contcols...)) &&
+        all(
+            col ->
+                haskey(block.categorydict, col) &&
+                    (ismissing(x[col]) || x[col] ∈ block.categorydict[col]),
+            block.catcols,
+        ) &&
+        all(col -> ismissing(x[col]) || x[col] isa Number, block.contcols)
+    )
+end
 
-# function mockblock(block::TextRow)
-# end
+function mockblock(block::TextRow)
+    cols = (block.catcols..., block.contcols...)
+    vals = map(cols) do col
+        col in block.catcols ? rand(block.categorydict[col]) : rand()
+    end
+    return NamedTuple(zip(cols, vals))
+end
 
-# function setup(::Type{TextRow}, data)
-# end
+"""
+    setup(TextRow, data[; catcols, contcols])
+
+Create a `TextRow` block from data container `data::TextDataset`. If the
+categorical and continuous columns are not specified manually, try to
+guess them from the dataset's column types.
+"""
+function setup(::Type{TextRow}, data; catcols=nothing, contcols=nothing)
+    catcols_, contcols_ = getcoltypes(data)
+    catcols = isnothing(catcols) ? catcols_ : catcols
+    contcols = isnothing(contcols) ? contcols_ : contcols
+
+    return TextRow(
+        catcols,
+        contcols,
+        gettransformdict(data, DataAugmentation.Categorify, catcols),
+    )
+end
+
+function Base.show(io::IO, block::TextRow)
+    print(io, ShowCase(block, (:catcols, :contcols), show_params=false, new_lines=true))
+end
 
 # ## Interpretation
 
-function showblock!(io, ::ShowText, block::TextRow, obs)
-end
+# function showblock!(io, ::ShowText, block::TextRow, obs) end
