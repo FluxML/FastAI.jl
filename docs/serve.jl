5d74129ed2526e363be1b71dc060d189ceebc506
lorenzoh <lorenz.ohly@gmail.com>
2022-04-27 09:39:40 +0200

    Update to new Pollen template

--- docs/serve.jl
+++ docs/serve.jl
@@ -1,13 +1,26 @@
+"""
+This script serves the Pollen.jl documentation on a local file server
+so that it can be loaded by the frontend in development mode.
+files should be stored.
+
+    > julia docs/serve.jl
+
+Use `./make.jl` to export the generated documents to disk.
+
+There are two modes for interactive development: Lazy and Regular.
+In lazy mode, each document will be built only if it is requested in
+the frontend, while for Regular mode, each document will be built
+once before serving.
+"""
+
 using Pollen
 
-# using MyPackage
-
-
-# Create Project
 project = include("project.jl")
 
 
-@info "Rewriting documents..."
-Pollen.rewritesources!(project)
-
-Pollen.serve(project, lazy=get(ENV, "POLLEN_LAZY", "false") == "true", format = Pollen.JSONFormat())
+Pollen.serve(
+    project;
+    lazy = get(ENV, "POLLEN_LAZY", "false") == "true",
+    port = Base.parse(Int, get(ENV, "POLLEN_PORT", "8000")),
+    format = JSONFormat()
+)
