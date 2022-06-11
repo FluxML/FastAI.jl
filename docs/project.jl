9e346f66ec552f52ddbdc0b46d8dfbd6dac6ab84
lorenzoh <lorenz.ohly@gmail.com>
2022-06-11 10:05:06 +0200

    Configure doc project for better live preview

--- docs/project.jl
+++ docs/project.jl
@@ -24,7 +24,7 @@ project = Project(
         StaticResources(),
         DocumentGraph(),
         SearchIndex(),
-        SaveAttributes((:title,), useoutputs=true),
+        SaveAttributes((:title,), useoutputs=false),
         LoadFrontendConfig(Pkg.pkgdir(m))
     ],
 )
