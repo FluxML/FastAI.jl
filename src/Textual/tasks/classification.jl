6d63b57717d846a50742ffd8dc2713526014882a
lorenzoh <lorenz.ohly@gmail.com>
2022-07-11 19:52:30 +0200

    remove old files again

--- src/Textual/tasks/classification.jl
+++
@@ -1,9 +0,0 @@
-function TextClassficationSingle(blocks::Tuple{<:Paragraph, <:Label}, data=nothing)
-    return SupervisedTask(
-        blocks,
-        (
-            
-            OneHot()
-        )
-    )
-end
