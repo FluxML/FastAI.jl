8e9f7aae1371535a8e62fa5e72df78432dad6b9a
Chandu-4444 <chandrakiran.g19@iiits.in>
2022-08-23 19:13:04 +0530

    Start text generation recipe for `imdb`

--- FastText/src/recipes.jl
+++ FastText/src/recipes.jl
@@ -34,12 +34,37 @@ function Datasets.loadrecipe(recipe::TextFolders, path)
     return data, blocks
 end
 
+Base.@kwdef struct TextGenerationFolders <: Datasets.DatasetRecipe
+    textgenerationfolder::String = "unsup"
+    split::Bool = false
+    filefilterfn = _ -> true
+end
+
+Datasets.recipeblocks(::Type{TextGenerationFolders}) = Tuple{Paragraph}
+
+function Datasets.loadrecipe(recipe::TextGenerationFolders, path)
+    isdir(path) || error("$path is not a directory")
+    textpath = joinpath(path, recipe.textgenerationfolder)
+    isdir(textpath) || error("$textpath is not a directory")
+    data = loadfolderdata(textpath,
+                          filterfn = f -> istextfile(f) && recipe.filefilterfn(f),
+                          loadfn = (loadfile),
+                          splitfn = recipe.split ? parentname : nothing)
+
+    (recipe.split ? length(data) > 0 : numobs(data) > 0) ||
+        error("No text files found in $textpath")
+    return data
+end
+
+
 # Registering recipes
 
-const RECIPES = Dict{String, Vector}("imdb" => [
-                                         TextFolders(filefilterfn = f -> !occursin(r"tmp_clas|tmp_lm|unsup|test",
-                                                                                   f)),
-                                     ])
+const RECIPES = Dict{String, Vector}("imdb" =>
+                                        [
+                                            TextFolders(filefilterfn = f -> !occursin(r"tmp_clas|tmp_lm|unsup|test", f)),
+                                            TextGenerationFolders(filefilterfn = f -> !occursin(r"tmp_clas|tmp_lm|test|rain", f)),
+                                        ],
+                                    )
 
 ## Tests
 
