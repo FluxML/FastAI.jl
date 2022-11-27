0a9353e49d79213239446bd5469c280b5f449632
lorenzoh <lorenz.ohly@gmail.com>
2022-11-27 13:11:34 +0100

    Add all Metalhead models

--- FastVision/src/blocks/convfeatures.jl
+++ FastVision/src/blocks/convfeatures.jl
@@ -1,5 +1,4 @@
 
-
 """
     ConvFeatures{N}(n) <: Block
     ConvFeatures(n, size)
@@ -12,12 +11,13 @@ struct ConvFeatures{N} <: Block
     size::NTuple{N, DimSize}
 end
 
+ConvFeatures{N}(n) where {N} = ConvFeatures{N}(n, ntuple(_ -> :, N))
 
-ConvFeatures{N}(n) where N = ConvFeatures{N}(n, ntuple(_ -> :, N))
-
-function FastAI.checkblock(block::ConvFeatures{N}, a::AbstractArray{T, M}) where {M,N,T}
-    M == N+1 || return false
+function FastAI.checkblock(block::ConvFeatures{N}, a::AbstractArray{T, M}) where {M, N, T}
+    M == N + 1 || return false
     return checksize(block.size, size(a))
 end
 
-FastAI.mockblock(block::ConvFeatures) = rand(Float32, map(l -> l isa Colon ? 8 : l, block.size)..., block.n)
+function FastAI.mockblock(block::ConvFeatures)
+    rand(Float32, map(l -> l isa Colon ? 8 : l, block.size)..., block.n)
+end
--- FastVision/src/modelregistry.jl
+++ FastVision/src/modelregistry.jl
@@ -1,25 +1,21 @@
 
-
 const _models = Dict{String, Any}()
 
 function cnn_variants(; nfeatures = :, hasweights = false)
     variants = Pair{String, ModelVariant}[]
 
-    hasweights && push!(variants, "imagenet_1k" => ModelVariant(
-            input=ImageTensor{2}(3),
-            # TODO: use actual ImageNet classes
-            output=FastAI.OneHotLabel{Int}(1:1000),
-        ))
-    push!(variants, "classifier" => ModelVariant(
-        make_cnn_classifier,
-        ImageTensor{2},
-        FastAI.OneHotTensor{0},
-    ))
-    push!(variants, "backbone" => ModelVariant(
-        make_cnn_backbone,
-        ImageTensor{2},
-        ConvFeatures{2}(nfeatures),
-    ))
+    hasweights && push!(variants,
+          "imagenet_1k" => ModelVariant(xblock = ImageTensor{2}(3),
+                                        # TODO: use actual ImageNet classes
+                                        yblock = FastAI.OneHotLabel{Int}(1:1000)))
+    push!(variants,
+          "classifier" => ModelVariant(make_cnn_classifier,
+                                       ImageTensor{2},
+                                       FastAI.OneHotTensor{0}))
+    push!(variants,
+          "backbone" => ModelVariant(make_cnn_backbone,
+                                     ImageTensor{2},
+                                     ConvFeatures{2}(nfeatures)))
 
     return variants
 end
@@ -30,20 +26,27 @@ function make_cnn_classifier(model, input::ImageTensor, output::OneHotTensor{0})
     return Chain(backbone, head)
 end
 
-function make_cnn_backbone(model, input::ImageTensor{N}, output::ConvFeatures{N}) where N
+function make_cnn_classifier(model, ::Type{Any}, ::Type{Any})
+    return model
+end
+
+function make_cnn_backbone(model, input::ImageTensor{N}, ::ConvFeatures{N}) where {N}
     backbone = _backbone_with_channels(model.layers[1], input.nchannels)
     return backbone
 end
 
+function make_cnn_backbone(model, ::Type{Any}, ::Type{Any})
+    return model.layers[1]
+end
+
 function _backbone_with_channels(backbone, n)
     layer = backbone.layers[1].layers[1]
-    layer isa Conv || throw(ArgumentError(
-        """To change the number of input channels,
-        `backbone.layers[1].layers[1]` must be a `Conv` layer."""))
+    layer isa Conv || throw(ArgumentError("""To change the number of input channels,
+                                          `backbone.layers[1].layers[1]` must be a `Conv` layer."""))
 
     sz = size(layer.weight)
-    ks = sz[begin:end-2]
-    in_, out = sz[end-1:end]
+    ks = sz[begin:(end - 2)]
+    in_, out = sz[(end - 1):end]
     in_ == n && return backbone
 
     layer = @set layer.weight = Flux.kaiming_normal(Random.GLOBAL_RNG, ks..., n, out)
@@ -51,9 +54,9 @@ function _backbone_with_channels(backbone, n)
 end
 
 function _head_with_classes(head, n)
-    head.layers[end] isa Dense || throw(ArgumentError(
-        """To change the number of output classes,
-        the last layer in head must be a `Dense` layer."""))
+    head.layers[end] isa Dense ||
+        throw(ArgumentError("""To change the number of output classes,
+                            the last layer in head must be a `Dense` layer."""))
     c, f = size(head[end].weight)
     if c == n
         # Already has correct number of classes
@@ -65,23 +68,83 @@ end
 
 function metalhead_loadfn(modelfn, args...)
     return function (checkpoint; kwargs...)
-        return modelfn(args...; pretrain=!isnothing(checkpoint), kwargs...)
+        return modelfn(args...; pretrain = !isnothing(checkpoint), kwargs...)
     end
 end
 
-for depth in (18,)
-    hasweights = true
-    nfeatures = 512
-    id = "metalhead/resnet$depth"
+# model config: id, description, basefn, variant, hasweights, nfeatures
+const METALHEAD_CONFIGS = [
+    ("metalhead/resnet18", "ResNet18", metalhead_loadfn(Metalhead.ResNet, 18), true, 512),
+    ("metalhead/resnet34", "ResNet34", metalhead_loadfn(Metalhead.ResNet, 34), true, 512),
+    ("metalhead/resnet50", "ResNet50", metalhead_loadfn(Metalhead.ResNet, 50), true, 2048),
+    ("metalhead/resnet101", "ResNet101", metalhead_loadfn(Metalhead.ResNet, 101), true,
+     2048),
+    ("metalhead/resnet152", "ResNet152", metalhead_loadfn(Metalhead.ResNet, 152), true,
+     2048),
+    ("metalhead/wideresnet50", "WideResNet50", metalhead_loadfn(Metalhead.WideResNet, 50),
+     true, 2048),
+    ("metalhead/wideresnet101", "WideResNet101",
+     metalhead_loadfn(Metalhead.WideResNet, 101), true, 2048),
+    ("metalhead/wideresnet152", "WideResNet152",
+     metalhead_loadfn(Metalhead.WideResNet, 152), true, 2048),
+    ("metalhead/googlenet", "GoogLeNet", metalhead_loadfn(Metalhead.GoogLeNet), false,
+     1024),
+    ("metalhead/inceptionv3", "InceptionV3", metalhead_loadfn(Metalhead.Inceptionv3), false,
+     2048),
+    ("metalhead/inceptionv4", "InceptionV4", metalhead_loadfn(Metalhead.Inceptionv4), false,
+     1536),
+    ("metalhead/squeezenet", "SqueezeNet", metalhead_loadfn(Metalhead.SqueezeNet), true,
+     512),
+    ("metalhead/densenet-121", "DenseNet121", metalhead_loadfn(Metalhead.DenseNet, 121),
+     false, 1024),
+    ("metalhead/densenet-161", "DenseNet161", metalhead_loadfn(Metalhead.DenseNet, 161),
+     false, 1472),
+    ("metalhead/densenet-169", "DenseNet169", metalhead_loadfn(Metalhead.DenseNet, 169),
+     false, 1664),
+    ("metalhead/densenet-201", "DenseNet201", metalhead_loadfn(Metalhead.DenseNet, 201),
+     false, 1920),
+    ("metalhead/resnext50", "ResNeXt50", metalhead_loadfn(Metalhead.ResNeXt, 50), true,
+     2048),
+    ("metalhead/resnext101", "ResNeXt101", metalhead_loadfn(Metalhead.ResNeXt, 101), true,
+     2048),
+    ("metalhead/resnext152", "ResNeXt152", metalhead_loadfn(Metalhead.ResNeXt, 152), true,
+     2048),
+    ("metalhead/mobilenetv1", "MobileNetV1", metalhead_loadfn(Metalhead.MobileNetv1), false,
+     1024),
+    ("metalhead/mobilenetv2", "MobileNetV2", metalhead_loadfn(Metalhead.MobileNetv2), false,
+     1280),
+    ("metalhead/mobilenetv3-small", "MobileNetV3 Small",
+     metalhead_loadfn(Metalhead.MobileNetv3, :small), false, 576),
+    ("metalhead/mobilenetv3-large", "MobileNetV3 Large",
+     metalhead_loadfn(Metalhead.MobileNetv3, :large), false, 960),
+    ("metalhead/efficientnet-b0", "EfficientNet-B0",
+     metalhead_loadfn(Metalhead.EfficientNet, :b0), false, 1280),
+    ("metalhead/efficientnet-b0", "EfficientNet-B0",
+     metalhead_loadfn(Metalhead.EfficientNet, :b0), false, 1280),
+    ("metalhead/efficientnet-b1", "EfficientNet-B1",
+     metalhead_loadfn(Metalhead.EfficientNet, :b1), false, 1280),
+    ("metalhead/efficientnet-b2", "EfficientNet-B2",
+     metalhead_loadfn(Metalhead.EfficientNet, :b2), false, 1280),
+    ("metalhead/efficientnet-b3", "EfficientNet-B3",
+     metalhead_loadfn(Metalhead.EfficientNet, :b3), false, 1280),
+    ("metalhead/efficientnet-b4", "EfficientNet-B4",
+     metalhead_loadfn(Metalhead.EfficientNet, :b4), false, 1280),
+    ("metalhead/efficientnet-b5", "EfficientNet-B5",
+     metalhead_loadfn(Metalhead.EfficientNet, :b5), false, 1280),
+    ("metalhead/efficientnet-b6", "EfficientNet-B6",
+     metalhead_loadfn(Metalhead.EfficientNet, :b6), false, 1280),
+    ("metalhead/efficientnet-b7", "EfficientNet-B7",
+     metalhead_loadfn(Metalhead.EfficientNet, :b7), false, 1280),
+    ("metalhead/efficientnet-b8", "EfficientNet-B8",
+     metalhead_loadfn(Metalhead.EfficientNet, :b8), false, 1280),
+]
+for (id, description, loadfn, hasweights, nfeatures) in METALHEAD_CONFIGS
     _models[id] = (;
-        id = id,
-        variants = cnn_variants(; hasweights, nfeatures),
-        checkpoints = hasweights ? ["imagenet1k"] : String[],
-        backend = :flux,
-        loadfn = metalhead_loadfn(Metalhead.ResNet, depth)
-    )
+                   id, loadfn, description,
+                   variants = cnn_variants(; hasweights, nfeatures),
+                   checkpoints = hasweights ? ["imagenet1k"] : String[],
+                   backend = :flux)
 end
-
 
 @testset "Model variants" begin
     @testset "make_cnn_classifier" begin
@@ -97,6 +160,9 @@ end
         m = Metalhead.ResNet(18)
         clf = make_cnn_backbone(m, ImageTensor{2}(10), ConvFeatures{2}(512))
         @test Flux.outputsize(clf, (256, 256, 10, 1)) == (8, 8, 512, 1)
-
     end
 end
+
+@testset "Metalhead models" begin for id in models(id = "metalhead")[:, :id]
+    @test_nowarn load(models()[id]; variant = "backbone")
+end end
